0af968b92dd7f25da4fed82782d16c7a790316ae
Hubert Yu <hubert12161999@gmail.com>
2022-05-27 15:31:04 +0100

    First push.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
+  # get 'bookmarks/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "lists#index"
+  root to: "lists#index"
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
+
 end
