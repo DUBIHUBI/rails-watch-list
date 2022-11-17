d8f94f66fcac25cd7c1af796e1ce74ab3128cc24
Hubert Yu <hubert12161999@gmail.com>
2022-11-17 11:45:00 +0000

    Remove uniqueness validation from overview

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  has_many :lists, through: :bookmarks
 
-  validates :title, :overview, uniqueness: true
+  validates :title, uniqueness: true
   validates :title, :overview, presence: true
 end
